84f7278b875a90709640beb2229b65a2f0054db0
Danil_Rybkin <danil.rybkin.19@gmail.com>
2021-12-16 23:32:54 +0300

    finish

---
+++ src/js/script.js
@@ -0,0 +1,6 @@
+window.addEventListener('DOMContentLoaded', () => {
+    $('.carousel__wrapper').slick({
+        prevArrow: '<button type="button" class="slick-prev"><img src="icon/arrow-left.svg" alt="arrow left" class="arrow-left"></button>',
+        nextArrow: '<button type="button" class="slick-next"><img src="icon/arrow-right.svg" alt="arrow right" class="arrow-right"></button>'
+    });
+});
